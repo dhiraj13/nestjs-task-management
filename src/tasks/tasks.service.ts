0b6edc3f261d4eb80b55d905550e69bb5de316d7
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-11-05 17:42:45 +0545

    persistence deleting a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -45,6 +45,14 @@ export class TasksService {
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksRepository.createTask(createTaskDto);
   }
+
+  async deleteTask(id: string): Promise<void> {
+    const result = await this.tasksRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+  }
   // deleteTask(id: string): void {
   //   const found = this.getTaskById(id);
   //   this.tasks = this.tasks.filter((task) => task.id !== found.id);
